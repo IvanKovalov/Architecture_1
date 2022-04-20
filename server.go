0b8d4a1c580dbdbdda955bbf26ea57eeb2328f00
IvanKovalov <kovalevvana8@gmail.com>
2022-04-20 18:43:45 +0300

    first commit

---
+++ server.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+	"time"
+
+	"github.com/go-chi/chi/v5"
+)
+
+type Date struct {
+	Hour time.Time `json:"поточний час"`
+}
+
+func main() {
+	r := chi.NewRouter()
+	r.Get("/time", func(w http.ResponseWriter, r *http.Request) {
+		data := Date{time.Now()}
+		js, _ := json.Marshal(data)
+		w.Write(js)
+	})
+	http.ListenAndServe(":8795", r)
+}
